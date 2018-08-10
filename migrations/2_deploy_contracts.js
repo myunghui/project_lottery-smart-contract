3849f1e2da8e94594492900f861f9c93a973b953
myunghui <37468137+myunghui@users.noreply.github.com>
2018-08-11 05:05:05 +0900

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,15 @@
+const ConvertLib        = artifacts.require("./ConvertLib.sol");
+const MetaCoin          = artifacts.require("./MetaCoin.sol");
 const IndexLottery     = artifacts.require("./IndexLottery.sol");
 const LotteryPayment = artifacts.require("./LotteryPayment.sol");
+
 module.exports = function(deployer) {
+	deployer.deploy(ConvertLib);
+	deployer.link(ConvertLib, MetaCoin);
+	deployer.deploy(MetaCoin);
 	deployer.deploy(LotteryPayment);
 	deployer.link(LotteryPayment, IndexLottery);
 	deployer.deploy(IndexLottery);
 };
+
+
