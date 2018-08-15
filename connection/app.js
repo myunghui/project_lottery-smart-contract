df092ddc856e4ccdfcb891278d1f16fae576c385
myunghui <37468137+myunghui@users.noreply.github.com>
2018-08-16 00:48:55 +0900

    Add files via upload

--- connection/app.js
+++ connection/app.js
@@ -36,7 +36,7 @@ module.exports = {
     let meta;
     MetaCoin.deployed().then(function(instance) {
       meta = instance;
-	  console.log(JSON.stringify(instance));
+	  //console.log(JSON.stringify(instance));
       return meta.getBalance.call(account, {from: account});
     }).then(function(value) {
         callback(value.valueOf());
@@ -46,24 +46,43 @@ module.exports = {
     });
   },
   
-  sendCoin: function(amount, sender, receiver, data, callback) {
-	const self = this;
+  //sendCoin: function(amount, sender, receiver, data, callback) {
+	sendCoin: function(callback) {
+		const self = this;
 
-	// Bootstrap the MetaCoin abstraction for Use.
-	MetaCoin.setProvider(self.web3.currentProvider);
+		self.web3.eth.getAccounts(function(err, accs) {
+		if (err != null) {
+			console.log("There was an error fetching your accounts.");
+			return;
+		}
 
-	let meta;
-	MetaCoin.deployed().then(function(instance) {
-		meta = instance;
-		console.log(JSON.stringify(instance));
-		return meta.sendCoin(receiver, amount, data, {from: sender});
-	}).then(function() {
-	  self.refreshBalance(sender, function (answer) {
-		callback(answer);
-	  });
-	}).catch(function(e) {
-		console.log(e);
-		callback("ERROR 404");
+		if (accs.length == 0) {
+			console.log("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+			return;
+		}
+		self.accounts = accs;
+		self.account = self.accounts[0];
+		console.log("self.account=" + self.account);
+		const sender = self.account;//"0x6a5d3e1f95da565fde011617b6804402b50924d6";
+		const receiver = "0xeca49d2a4ad1a01412f57736efc6f40765e5b56d";
+		const amount = 1000000000000000000;
+		const data = "5678";
+		MetaCoin.setProvider(self.web3.currentProvider);
+		let meta;
+		MetaCoin.deployed().then(function(instance) {
+			meta = instance;
+			// address receiver, uint amount, string data
+			//console.log(JSON.stringify(instance));
+			return meta.sendCoin(receiver, amount, data, {from: sender});
+		}).then(function() {
+		  self.refreshBalance(sender, function (answer) {
+			callback(answer);
+		  });
+		}).catch(function(e) {
+			console.log(e);
+			callback("ERROR 404");
+		});
+
 	});
 	
   }
--- connection/app2.js
+++ connection/app2.js
@@ -3,7 +3,7 @@ const BigNumber = require("bignumber.js");
 
 const lottery_artifact = require("../build/contracts/IndexLottery.json");
 const Lottery = contract(lottery_artifact);
-const ADDR_LOTTERY = "0xc7f8f3397ca9e25dd5ee69eca2c271ece6ec27cb";
+const ADDR_LOTTERY = "0x0b7919440a9d6cc13333f95891599c90b42455f2";
 
 
 module.exports = {
@@ -26,7 +26,10 @@ module.exports = {
 				return;
 			}
 			
+			console.log(">>>	0:" + accs[0]);
+			
 			callback(accs);
+
 		
 		});
 		
@@ -55,18 +58,18 @@ module.exports = {
 
 	},
 	
-	getCurrentBalance : function(callback) {
+	getBalance : function(callback) {
 		
 		const self = this;
 		const address = ADDR_LOTTERY;
-		console.log("###	[getCurrentBalance]	address=" + address);
+		console.log("###	[getBalance]	address=" + address);
 		
 		Lottery.setProvider(self.web3.currentProvider);
 
-		let meta;
-		Lottery.deployed().then(function(instance) {
-			meta = instance;
-			return meta.getBalance.call(address, {from: address});
+		let instance;
+		Lottery.deployed().then(function(_instance) {
+			instance = _instance;
+			return instance.getBalance.call(address, {from: address});
 		}).then(function(value) {
 			const amount = self.web3.fromWei(value.valueOf(), "ether");
 			callback(address, amount);
@@ -80,62 +83,63 @@ module.exports = {
 		
 		const self = this;
 		const receiver = ADDR_LOTTERY;
+		//sender = "0x7d7a8bdd19e3cb5e56f4b431a03df65625da1022";
+		
 		const weiAmt =  BigNumber(self.web3.toWei(amount, "ether")).toFixed();
-
-		console.log("###	[deposit]	amount=" + weiAmt + ", sender=" + sender + ", receiver=" + ADDR_LOTTERY);
+		
+		console.log("###	[deposit]	amount=" + weiAmt + ", sender=" + sender + ", receiver=" + receiver);
 		
 		Lottery.setProvider(self.web3.currentProvider);
-
-		let meta;
-		Lottery.deployed().then(function(instance) {
-			meta = instance;
-			return meta.transferFrom.sendTransaction(
-				sender, receiver, weiAmt, 
-				{from:sender},
-				function(err, result) {
-					if (error != null) {
-						console.log(error);
-						return;
-					}
-					console.log("deposit	=>	" + result);
+		
+		self.web3.eth.sendTransaction(
+			{from:sender,to:receiver,value:weiAmt},
+			function(error, result) {
+				if (error != null) {
+					console.log(error);
+					return;
 				}
+				console.log("result=" + result);
 				
-				);
-		  
-			/*
-			return
-			self.web3.eth.sendTransaction(
-				{
-					from:sender,
-					to:receiver,
-					value:weiAmt
-				},
-				function(err, result) {
-					if (error != null) {
-						console.log(error);
-						return;
-					}
-					console.log("deposit	=>	" + result);
-				}
-			);
-			 */
-		  
-		  
-		  
-		}).then(function() {
-//			self.getCurrentBalance(function (address, amount) {
-//				callback(address, amount);
-//			});
-//		}).then(function() {
-//			self.getAccountBalance(sender, function (address, amount) {
-//				callback(address, amount);
-//			});
-		}).catch(function(e) {
-			console.log(e);
-			callback("ERROR 404");
-		});
+				let instance;
+				let from_balance;
+				let to_balance;
+				
+				let from_wallet_balance;
+				let to_wallet_balance;
+				Lottery.deployed().then(function(_instance) {
+					instance = _instance;
+					return instance.getBalance.call(sender, {from: sender});
+				}).then(function(weiValue) {
+					from_balance = self.web3.fromWei(weiValue.valueOf(), "ether");
+					return instance.getBalance.call(receiver, {from: receiver});
+				}).then(function(weiValue) {
+					to_balance = self.web3.fromWei(weiValue.valueOf(), "ether");
+					self.ethGetBalance(
+						sender,
+						function(value) {
+							from_wallet_balance = value;
+							console.log("$$$$	" + sender + ":" + from_wallet_balance + "			" + from_balance);
+						}
+					);
+				}).then(function() {
+					self.ethGetBalance(
+						receiver,
+						function(value) {
+							to_wallet_balance = value;
+							console.log("$$$$	" + receiver + ":" + to_wallet_balance + "		" + to_balance);
+						}
+					);
+				}).then(function() {
+					callback(from_balance, to_balance);
+				}).catch(function(e) {
+					console.log(e);
+				});
+				
+			}
+		);
 		
 	},
+	
 	
 	buy : function(sender, count, data, gas, callback) {
 		
@@ -143,37 +147,24 @@ module.exports = {
 		const gasPrice = BigNumber(gas).multipliedBy(1000000000);
 		const amount = self.web3.toWei(BigNumber(0.02).multipliedBy(count).toFixed(), "ether");
 		const receiver = ADDR_LOTTERY;
+		let tranArg = {from:sender, value:amount, data:data};
 		
 		console.log("###	[buy]	count=" + count + ", amount=" + amount + ", sender=" + sender + ", receiver=" + receiver + ", data=" + data);
 		
-		
 		Lottery.setProvider(self.web3.currentProvider);
-
-		let meta;
-		Lottery.deployed().then(function(instance) {
-			meta = instance;
-			
-			return
-			self.web3.eth.sendTransaction(
-				{
-					from:sender,
-					to:receiver,
-					value:amount,
-					gasPrice:gasPrice
-				},
-				function(err, result) {
-					if (error != null) {
-						console.log(error);
-						return;
-					}
-					console.log(result);
-				}
-			);
-			
-		}).then(function() {
-//			self.getCurrentBalance(sender, function (address, amount) {
-//				callback(amount);
-//			});
+		
+		let instance;
+		Lottery.deployed().then(function(_instance) {
+			instance = _instance;
+			//console.log(">>>	instance=" + JSON.stringify(instance));
+			console.log(">>>	instance=" + instance.address);
+			return instance.buyLottery.sendTransaction(tranArg.value, tranArg.data, tranArg.from, tranArg);
+		}).then(function(result) {
+			console.log(">>>	buyLottery=" + result);
+			return instance.getBalance.call();
+		}).then(function(result) {
+			console.log(">>>	getBalance=" + result);
+			callback(result);
 		}).catch(function(e) {
 			console.log(e);
 			callback("ERROR 404");
